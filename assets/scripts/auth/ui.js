076f0353e0c427ff2b246823740751923e95c228
paulkleinschmidt <paul.kleinschmidt1@gmail.com>
2017-11-29 15:18:25 -0500

    Adds functioning authorizition forms
    Sign in, sign up, change pass, sign out

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,45 @@
+const store = require('../store')
+
+const signUpSuccess = function () {
+  $('#message').text('Signed up successfully!')
+}
+
+const signUpFailure = function () {
+  $('#message').text('Error on sign up')
+}
+
+const signInSuccess = function (data) {
+  $('#message').text('Signed in Successfully')
+  store.user = data.user
+}
+
+const signInFailure = function () {
+  $('#message').text('sign in failed')
+}
+
+const changePasswordFailure = function () {
+  $('#message').text('password change failed')
+}
+
+const changePasswordSuccess = function (data) {
+  $('#message').text('password change successful')
+}
+
+const signOutSuccess = function () {
+  $('#message').text('Signed Out Successfully')
+}
+
+const signOutFailure = function () {
+  $('#message').text('Sign Out Failed')
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,8 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://floating-lake-78080.herokuapp.com/'
+    production: 'https://floating-lake-78080.herokuapp.com',
+    development: 'http://localhost:4741'
   }
 }
 
